10a56af56cd61d898075243b87e5b343c212c963
Igor Sushko <46295818+IgorSushko1@users.noreply.github.com>
2020-08-24 12:02:11 +0500

    Переустановливал windows
    Полетел загрузочный сектор на диске. После восстановления добавил в package следующие пакеты:
    babel,
    webpack,
    и указал определенные версии vue и его дочерних пакетов.
    Это было сделано чтобы исключить ошибки при работе dev-server после загрузки проекта с github.

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,131 +1,129 @@
-import colors from 'vuetify/es5/util/colors'
+// import colors from 'vuetify/es5/util/colors'
 import axios from 'axios'
 
 // const list = ['aboutFilm/1', 'aboutFilm/1', 'aboutFilm/1']
-const list = async () => {
-  const res = await axios.get('http://localhost:3000/api/films')
-  console.log(res.data)
-  return res.data.map((el) => `aboutFilm/${el._id}`)
+const list = async() => {
+    const res = await axios.get('http://localhost:3000/api/films')
+    console.log(res.data)
+    return res.data.map((el) => `aboutFilm/${el._id}`)
 }
 export default {
-  /*
-   ** Nuxt rendering mode
-   ** See https://nuxtjs.org/api/configuration-mode
-   */
-  mode: 'universal',
-  /*
-   ** Nuxt target
-   ** See https://nuxtjs.org/api/configuration-target
-   */
-  target: 'server',
-  /*
-   ** Headers of the page
-   ** See https://nuxtjs.org/api/configuration-head
-   */
-  head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-    ],
-    link: [
-      {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico',
-      },
-    ],
-  },
-
-  generate: { routes: list },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   ** https://nuxtjs.org/guide/plugins
-   */
-  // plugins: ['~/plugins/api-context.js'],
-  /*
-   ** Auto import components
-   ** See https://nuxtjs.org/api/configuration-components
-   */
-  components: true,
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/auth',
-    '@nuxt/http',
-  ],
-
-  http: {
-    // proxyHeaders: false
-  },
-
-  // serverMiddleware: [
-  //   {
-  //     path: '/api',
-  //     handler: require('body-parser').json(),
-  //   },
-  //   {
-  //     path: '/api',
-  //     handler: (req, res, next) => {
-  //       req.query = new URL(req.url)
-  //       req.params = {
-  //         ...req.query,
-  //         ...req.body,
-  //       }
-  //       next()
-  //     },
-  //   },
-  //   {
-  //     path: '/api',
-  //     handler: '~/serverMiddleware/api-server.js',
-  //   },
-  // ],
-  serverMiddleware: { '/api': '~/api' },
-
-  /*
-   ** vuetify module configuration
-   ** https://github.com/nuxt-community/vuetify-module
-   */
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      // dark: true,
-      // themes: {
-      //   dark: {
-      //     primary: colors.blue.darken2,
-      //     accent: colors.grey.darken3,
-      //     secondary: colors.amber.darken3,
-      //     info: colors.teal.lighten1,
-      //     warning: colors.amber.base,
-      //     error: colors.deepOrange.accent4,
-      //     success: colors.green.accent3,
-      //   },
-      // },
+    /*
+     ** Nuxt rendering mode
+     ** See https://nuxtjs.org/api/configuration-mode
+     */
+    mode: 'universal',
+    /*
+     ** Nuxt target
+     ** See https://nuxtjs.org/api/configuration-target
+     */
+    target: 'server',
+    /*
+     ** Headers of the page
+     ** See https://nuxtjs.org/api/configuration-head
+     */
+    head: {
+        titleTemplate: '%s - ' + process.env.npm_package_name,
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || '',
+            },
+        ],
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico',
+        }, ],
     },
-  },
-  /*
-   ** Build configuration
-   ** See https://nuxtjs.org/api/configuration-build/
-   */
-  build: {},
+
+    generate: { routes: list },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     ** https://nuxtjs.org/guide/plugins
+     */
+    // plugins: ['~/plugins/api-context.js'],
+    /*
+     ** Auto import components
+     ** See https://nuxtjs.org/api/configuration-components
+     */
+    components: true,
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/auth',
+        '@nuxt/http',
+    ],
+
+    http: {
+        // proxyHeaders: false
+    },
+
+    // serverMiddleware: [
+    //   {
+    //     path: '/api',
+    //     handler: require('body-parser').json(),
+    //   },
+    //   {
+    //     path: '/api',
+    //     handler: (req, res, next) => {
+    //       req.query = new URL(req.url)
+    //       req.params = {
+    //         ...req.query,
+    //         ...req.body,
+    //       }
+    //       next()
+    //     },
+    //   },
+    //   {
+    //     path: '/api',
+    //     handler: '~/serverMiddleware/api-server.js',
+    //   },
+    // ],
+    serverMiddleware: { '/api': '~/api' },
+
+    /*
+     ** vuetify module configuration
+     ** https://github.com/nuxt-community/vuetify-module
+     */
+    vuetify: {
+        customVariables: ['~/assets/variables.scss'],
+        theme: {
+            // dark: true,
+            // themes: {
+            //   dark: {
+            //     primary: colors.blue.darken2,
+            //     accent: colors.grey.darken3,
+            //     secondary: colors.amber.darken3,
+            //     info: colors.teal.lighten1,
+            //     warning: colors.amber.base,
+            //     error: colors.deepOrange.accent4,
+            //     success: colors.green.accent3,
+            //   },
+            // },
+        },
+    },
+    /*
+     ** Build configuration
+     ** See https://nuxtjs.org/api/configuration-build/
+     */
+    build: {},
 }
